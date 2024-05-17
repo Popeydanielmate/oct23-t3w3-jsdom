ad33da735b764c4f67c989f8c14c3246a0d0aeef
Popeydanielmate <14269@coderacademy.edu.au>
2024-05-17 19:47:37 +1000

    feat: toggle CSS function theme implemented

--- themeControl.js
+++ themeControl.js
@@ -17,4 +17,27 @@ function changeCssTheme(themeName){
     }
 }
 
+function setThemeToDark(){
+    changeCssTheme("dark");
+}
+
+function setThemeToLight(){
+    changeCssTheme("light");
+}
+
+function toggleCssTheme(){
+    let currentBackgroundColor = getComputedStyle(document.documentElement).getPropertyValue("--background");
+
+    console.log("Current background color is: " + currentBackgroundColor);
+
+    if (currentBackgroundColor == "white"){
+        // we are already in light theme, set it to dark
+    } else {
+        // we are in dark theme, set it to light
+    }
+}
+
+
+
+
 changeCssTheme("dark");
